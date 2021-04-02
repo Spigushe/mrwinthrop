59150f284132e93c051baafb997d8bd4afdddcd0
Spigushe <martin.cuchet@gmail.com>
2021-04-02 18:43:42 +0200

    Command builds in terminal

--- mrw_bot/__init__.py
+++ mrw_bot/__init__.py
@@ -14,14 +14,15 @@ from discord.ext import commands
 """
 if intents become necessary at some point:
 	intents = discord.Intents.all()
-	bot = commands.Bot(command_prefix='mrw', intents=intents)
+	bot = commands.Bot(command_prefix=commands.when_mentioned_or('mrw '), intents=intents)
 """
-bot = commands.Bot(command_prefix='mrw ')
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('mrw '))
 
 from krcg import logging
 from krcg import analyzer
 from krcg import vtes
-from krcg_cli.subcommands import *
+from krcg_cli import parser
+#from krcg_cli.subcommands import *
 
 logger = logging.logger
 client = discord.Client()
@@ -32,17 +33,31 @@ async def on_ready():
 	"""Login success informative log"""
 	logger.info("Logged in as {}", bot.user)
 
+
 """Config by commands"""
 @bot.command(
 	name="build",
-	aliases=["builds"],
-	help="Build a deck from any given cards based on TWDA, takes any number of card names to build a deck sample",
-	brief="Build a deck from any given cards based on TWDA",
-	usage="mrw build \"Fame\" \"Carrion Crows\"",
+	#aliases=["builds"],
+	help="Build a deck from any given card(s) based on TWDA, takes any number of card names to build a deck sample",
+	brief="Build a deck from any given card(s) based on TWDA",
+	usage="Fame|Carrion Crows",
 )
 async def msg_build(ctx, *args):
 	logger.info("Received instructions {}", ctx.message.content)
-	build.build(args)
+
+	card_list = ctx.message.content[10:]
+	cards = card_list.split("|") if "|" in card_list else card_list
+	logger.info("Cards: {}", cards)
+
+	cmd = ['build']
+	if len(cards) > 1:
+		cmd = cmd + cards
+	else:
+		cmd = cmd + cards.split()
+	print(cmd)
+
+	parser.execute(cmd)
+
 
 def main():
 	"""Entrypoint for the Discord Bot"""
