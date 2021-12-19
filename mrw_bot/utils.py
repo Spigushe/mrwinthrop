6a02120fa1c2d4292d4059f637d52533089adb89
Spigushe <martin.cuchet@gmail.com>
2021-12-19 10:16:20 +0100

    Fix filter_twda

--- mrw_bot/utils.py
+++ mrw_bot/utils.py
@@ -15,10 +15,13 @@ def normalize(s):
 
 def filter_twda(args):
     decks = list(twda.TWDA.values())
-    if args["date_from"]:
-        decks = [d for d in decks if d.date >= args["date_from"]]
-    if args["date_to"]:
-        decks = [d for d in decks if d.date < args["date_to"]]
-    if args["players"]:
-        decks = [d for d in decks if d.players_count >= args["players"]]
+    date_from = args.pop("date_from", None)
+    if date_from:
+        decks = [d for d in decks if d.date >= date_from]
+    date_to = args.pop("date_to", None)
+    if date_to:
+        decks = [d for d in decks if d.date < date_to]
+    players = args.pop("players", None)
+    if players:
+        decks = [d for d in decks if (d.players_count or 0) >= players]
     return decks
